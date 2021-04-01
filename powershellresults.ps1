55b6f564ca9ecdd039566cf4929577120bd3c47f
emperornerd <81482033+emperornerd@users.noreply.github.com>
2021-04-01 17:45:29 -0600

    Delete powershellresults.ps1

--- powershellresults.ps1
+++
@@ -1,5 +0,0 @@
-$UserDesktop = (Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Desktop").Desktop
-
-get-winevent -FilterHashTable @{logname="Application"; id="1001"}| ?{$_.providername -match "wininit"} | fl timecreated, message
-
-get-winevent -FilterHashTable @{logname="Application"; id="1001"}| ?{$_.providername -match "wininit"} | fl timecreated, message | out-file "$UserDesktop\CHKDSKResults.txt"
